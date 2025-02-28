0cf908f3352c7d97143fb55d620bb27a3aecb2a3
Oliver Hughes <oliver.g.hughes@gmail.com>
2025-02-28 17:15:39 +0000

    Debgging for heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=7od=rzep8n8n7=*4%znhiebgfg&#l71=klt6(&^e=e3%ov2ka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','heroku.com']
+ALLOWED_HOSTS = ['127.0.0.1','heroku.com','herokuapp.com','localhost']
 
 # Application definition
 
