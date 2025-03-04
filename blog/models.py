d6cd23c7e02d559e4f0fefa04b35bef53de58280
Olbotron <oliver.g.hughes@gmail.com>
2025-03-04 11:39:06 +0000

    Work on comments

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,7 @@ class Post(models.Model):
     def __str__(self):
         return f"title: {self.title} | author: {self.author}"
     
+    
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
