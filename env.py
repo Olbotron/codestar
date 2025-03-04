2762efd69ac3178272f9233a41487824ff6bd2d6
Olbotron <oliver.g.hughes@gmail.com>
2025-03-04 08:42:55 +0000

    Update: can now edit comments

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_agBPW2yIet7L@ep-sparkling-dust-a2on2qzk.eu-central-1.aws.neon.tech/scuba_matt_angel_920033")
+
+SECRET_KEY = "revorgrepus#Hi!1984"
